c969e19c9107a85aacb2bff67666541bff8a9752
Juliette Cordor <professional@maybejules.com>
2024-11-04 17:36:42 +1100

    added v2 structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod v2 {
+    use serde::{Deserialize, Serialize};
+    use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    pub const DATABASE_URL: &str = "https://raw.githubusercontent.com/marticliment/UniGetUI/main/WebBasedData/screenshot-database-v2.json";
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn database_url() -> url::Url {
+        unsafe { url::Url::parse(DATABASE_URL).unwrap_unchecked() }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub struct ScreenshotDatabase {
+        package_count: PackageCount,
+        packages: HashMap<String, PackageImages>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub struct PackageImages {
+        icon: url::Url,
+        images: Vec<url::Url>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub struct PackageCount {
+        total: u64,
+        done: u64,
+        packages_with_icon: u64,
+        packages_with_screenshot: u64,
+        total_screenshots: u64,
     }
 }
+
+pub use v2::*;
